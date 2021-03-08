227581c5708f6ecc64ff7f12caf3921e6c8447b8
bannholtzer jonathan <bannholtzer.j@gmail.com>
2021-03-08 19:39:43 +0100

    Menu

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const btnMenu = document.querySelector(".btnMenu");
+const btnSwitch = document.querySelector(".btnMenu");
 const nav = document.querySelector(".menu");
 const allItemNav = document.querySelectorAll(".itemMenu");
-const ligne = documet.querySelector(".contLign");
+const ligne = document.querySelector(".contLign");
 
-btnMenu.addEventListener("click", () => {
+btnSwitch.addEventListener("click", () => {
   ligne.classList.toggle("active");
+  nav.classList.toggle("menu-visible");
 });
+
+if (window.matchMedia("(max-width:1300px)")) {
+  allItemNav.forEach((item) => {
+    item.addEventListener("click", () => {
+      nav.classList.toggle("menu-visible");
+      ligne.classList.toggle("active");
+    });
+  });
+}
