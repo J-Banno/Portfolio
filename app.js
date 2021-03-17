e39611f56ddc0a16827bd192b33f0853e4a15e5c
bannholtzer jonathan <bannholtzer.j@gmail.com>
2021-03-17 19:44:58 +0100

    typeWriter

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Menu Burger//
 const btnSwitch = document.querySelector(".btnMenu");
 const nav = document.querySelector(".menu");
 const allItemNav = document.querySelectorAll(".itemMenu");
@@ -16,3 +17,33 @@ if (window.matchMedia("(max-width:1300px)")) {
     });
   });
 }
+
+//TypeWriter//
+
+const txtAnim = document.querySelector(".txt-animation");
+
+let typewriter = new Typewriter(txtAnim, {
+  loop: true,
+  deleteSpeed: 20,
+});
+
+typewriter
+  .pauseFor(1800)
+  .changeDelay(30)
+  .typeString("Je suis Jonathan Bannholtzer, ")
+  .pauseFor(300)
+  .typeString("<strong>Développeur Full-Stack</strong> !")
+  .pauseFor(1000)
+  .deleteChars(12)
+  .typeString('<span style="color: #7319f0;"> HTML/CSS</span> !')
+  .pauseFor(1000)
+  .deleteChars(10)
+  .typeString('<span style="color: #f0db19;"> JavaScript</span> !')
+  .pauseFor(1000)
+  .deleteChars(12)
+  .typeString('<span style="color: #1f8fbd;"> React Js</span> !')
+  .pauseFor(1000)
+  .deleteChars(11)
+  .typeString('<span style="color: #4ac22c;"> NodeJS-MongoDB</span> !')
+  .pauseFor(1000)
+  .start();
